590552c6125170d0a8a2fd0dc9c7ecae5fde0e03
FilippKramer <krame490@gmail.com>
2018-12-24 13:33:25 -0600

    Connect to mongoDB

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const db = require('./config/keys').mongoURI;
 
 const app = express();
 const port = process.env.PORT || 5000;
+mongoose
+  .connect( db, { useNewUrlParser: true } )
+  .then( () => console.log("Successfully connected to mongoDB"))
+  .catch(err => console.log(err))
 
 app.get("/", (req, res) => res.send("Hello world!"));
 
