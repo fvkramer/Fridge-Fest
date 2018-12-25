1d16e05d537dcbe7e585d0b3d51b04c10bacd6ea
FilippKramer <krame490@gmail.com>
2018-12-25 12:38:47 -0600

    Add registration validation

---
+++ validation/register.js
@@ -0,0 +1,36 @@
+const Validator = require("validator");
+const validText = require("./valid-text");
+
+module.exports = function validateRegisterInput(data) {
+  let errors = {};
+
+
+  data.username = validText(data.username) ? data.username : '';
+  data.password = validText(data.password) ? data.password : '';
+  data.password2 = validText(data.password2) ? data.password2 : '';
+
+  if (!Validator.isLength(data.username, { min: 3, max: 30})) {
+    errors.handle = 'Username must be between 3 and 30 characters'
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password field is required";
+  }
+
+  if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+    errors.password = "Password must be at least 6 characters";
+  }
+
+  if (Validator.isEmpty(data.password2)) {
+    errors.password2 = "Confirm Password field is required";
+  }
+
+  if (!Validator.equals(data.password, data.password2)) {
+    errors.password2 = "Passwords must match";
+  }
+
+  return {
+    errors,
+    isValid: Object.keys(errors).length === 0
+  };
+};
