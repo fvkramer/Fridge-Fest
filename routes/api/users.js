f917c0ddaeafba044a7cbd249aae91b6493b531d
FilippKramer <krame490@gmail.com>
2018-12-25 10:36:11 -0600

    Add backend signup with bcrypt encryption

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,34 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const User = require("../../models/User");
+
 
 router.get("/test", (req, res) => res.json({msg: "User has entered the game"}))
+
+router.post("/register", (req, res) => {
+  User.findOne({username: req.body.username})
+    .then( user => {
+      if (user) {
+        return res.status(400).json({username: "Username taken"})
+      } else {
+        newUser = new User({
+          username: req.body.username,
+          password: req.body.password
+        })
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser.save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err))
+          })
+        })
+      }
+    })
+})
+
 
 module.exports = router;
