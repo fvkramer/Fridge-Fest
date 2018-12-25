acc7317da737dfb74f81e976aa44bcb1fe6b211b
bismuth1991 <pbao.tran.unsw@gmail.com>
2018-12-25 01:24:44 -0800

    Create sessionReducer

---
+++ frontend/src/reducers/session_reducer.js
@@ -0,0 +1,22 @@
+import { RECEIVE_CURRENT_USER, RECEIVE_USER_LOGOUT } from '../actions/session_actions';
+
+const initialState = {
+  isAuthenticated: false,
+  user: undefined,
+};
+
+const sessionReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      return {
+        isAuthenticated: !!action.currentUser,
+        user: action.currentUser,
+      };
+    case RECEIVE_USER_LOGOUT:
+      return initialState;
+    default:
+      return state;
+  }
+};
+
+export default sessionReducer;
