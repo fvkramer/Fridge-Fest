53c001fd71db4e23a38f376514e7f4aafcded1f2
bismuth1991 <pbao.tran.unsw@gmail.com>
2018-12-25 00:37:14 -0800

    Write #signup, #login, and #setAuthToken session_api_util

---
+++ frontend/src/util/session_api_util.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const setAuthToken = (token) => {
+  if (token) {
+    axios.dafaults.headers.common.Authorization = token;
+  } else {
+    delete axios.defaults.headers.common.Authorization;
+  }
+};
+
+export const signup = userData => axios.post(
+  '/api/users/signup',
+  userData,
+);
+
+export const login = userData => axios.post(
+  'api/users/login',
+  userData,
+);
