2dac31c068143c097ecf12348d403d5cfccf93ee
bismuth1991 <pbao.tran.unsw@gmail.com>
2018-12-24 23:42:37 -0800

    Setup skeleton for app.jsx

---
+++ frontend/src/components/app.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Switch } from 'react-router-dom';
+import { AuthRoute } from '../util/route_util';
+
+import MainPage from './main/main_page';
+import LoginFormContainer from './session/login_form_container';
+import SignupFormContainer from './session/signup_form_container';
+
+const App = () => (
+  <>
+    <Switch>
+      <AuthRoute exact path="/" component={MainPage} />
+      <AuthRoute exact path="/login" component={LoginFormContainer} />
+      <AuthRoute exact path="/signup" component={SignupFormContainer} />
+    </Switch>
+  </>
+);
+
+export default App;
