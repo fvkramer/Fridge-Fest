5512735dbd54b54e1dcd9a7a54382c3b096a0cf6
FilippKramer <krame490@gmail.com>
2018-12-27 10:45:53 -0600

    Create httpServer instance on backend and wrap instance in socket.io

--- index.js
+++ index.js
@@ -7,7 +7,16 @@ const passport = require('passport');
 const users = require('./routes/api/users');
 
 const app = express();
+const httpServer = require("http").Server(app)
+const io = require("socket.io")(httpServer);
+
+io.on('connection', socket => {
+  console.log("Socket connected correctly");
+})
+
 const port = process.env.PORT || 5000;
+httpServer.listen(port, () => console.log(`Server is running on port ${port}`));
+
 mongoose
   .connect( db, { useNewUrlParser: true } )
   .then( () => console.log("Successfully connected to mongoDB"))
@@ -20,5 +29,3 @@ app.get("/", (req, res) => res.send("Fridge Fest is Live!"));
 app.use(passport.initialize());
 require('./config/passport')(passport);
 app.use('/api/users', users);
-
-app.listen(port, () => console.log(`Server is running on port ${port}`));
