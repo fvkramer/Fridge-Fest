f5107d8005c33586f698b989b3b0d5194d40353b
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-01-03 11:02:39 -0800

    Fix bug in Heroku setup

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,9 +1,4 @@
-// module.exports = {
-//   mongoURI: 'mongodb://krame490:go_student123@ds029267.mlab.com:29267/fridgefest',
-//   secretOrKey: 'fridgefestsignature',
-// };
-
 module.exports = {
-  mongoURI: ProcessingInstruction.env.MONGO_URI,
-  secretOrKey: ProcessingInstruction.env.SECRET_OR_KEY,
+  mongoURI: process.env.MONGO_URI,
+  secretOrKey: process.env.SECRET_OR_KEY,
 };
