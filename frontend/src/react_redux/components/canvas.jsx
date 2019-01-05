3da581f139c2299c2c418538d0a582d7daad7578
FilippKramer <krame490@gmail.com>
2019-01-05 11:44:53 -0800

    Initial special_ability commit

--- frontend/src/react_redux/components/canvas.jsx
+++ frontend/src/react_redux/components/canvas.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 
+// const HEIGHT = `${window.innerHeight}px`;
+// const WIDTH = `${window.innerWidth}px`;
+
 export default class Canvas extends Component {
   render() {
     return (
       <canvas
         id="canvas"
-        height="500px"
-        width="500px"
+        height="800px"
+        width="800px"
         style={{ border: '1px solid black' }}
       />
     );
