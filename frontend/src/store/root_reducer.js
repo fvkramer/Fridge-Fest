84b5211d177424e00a17bc237fa9f0c16a6e4d67
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-01-06 12:21:52 -0800

    Merge modalReducer => uiReducer => rootReducer

--- frontend/src/store/root_reducer.js
+++ frontend/src/store/root_reducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 import sessionReducer from '../react_redux/reducers/session_reducer';
 import errorsReducer from '../react_redux/reducers/errors_reducer';
 import gameReducer from '../game/reducers/game_reducer';
+import uiReducer from '../react_redux/reducers/uiReducer';
 
 const rootReducer = combineReducers({
   session: sessionReducer,
   errors: errorsReducer,
+  ui: uiReducer,
   game: gameReducer,
 });
 
