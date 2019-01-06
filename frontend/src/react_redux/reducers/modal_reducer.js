4b16acb47537a7ffe122b8d4533d8cca1a3ab209
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-01-06 12:19:13 -0800

    Create modalReducer

---
+++ frontend/src/react_redux/reducers/modal_reducer.js
@@ -0,0 +1,12 @@
+const modalReducer = (state, action) => {
+  switch (action.type) {
+    case 'OPEN_MODAL':
+      return action.modal;
+    case 'CLOSE_MODAL':
+      return null;
+    default:
+      return state;
+  }
+};
+
+export default modalReducer;
