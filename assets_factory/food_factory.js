027ed752e1e3f54300f722d327d45f19e5dae619
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-01-07 08:31:55 -0800

    Implement makeXY to food and skill facroties

--- assets_factory/food_factory.js
+++ assets_factory/food_factory.js
@@ -1,43 +1,53 @@
-const randomFromRange = require('./asset_factory_util');
+const { randomFromRange, makeXY } = require('./asset_factory_util');
 
 const instantRamen = [];
 for (let i = 0; i < 5; i += 1) {
   instantRamen.push({
     id: `food-ramen-${i}`,
-    x: randomFromRange(0, 2000),
-    y: randomFromRange(0, 2000),
+    // x: randomFromRange(0, 2000),
+    // y: randomFromRange(0, 2000),
+    x: makeXY(),
+    y: makeXY(),
   });
 }
 const pizza = [];
 for (let i = 0; i < 20; i += 1) {
   pizza.push({
     id: `food-pizza-${i}`,
-    x: randomFromRange(0, 2000),
-    y: randomFromRange(0, 2000),
+    // x: randomFromRange(0, 2000),
+    // y: randomFromRange(0, 2000),
+    x: makeXY(),
+    y: makeXY(),
   });
 }
 const donut = [];
 for (let i = 0; i < 15; i += 1) {
   donut.push({
     id: `food-donut-${i}`,
-    x: randomFromRange(0, 2000),
-    y: randomFromRange(0, 2000),
+    // x: randomFromRange(0, 2000),
+    // y: randomFromRange(0, 2000),
+    x: makeXY(),
+    y: makeXY(),
   });
 }
 const milkshake = [];
 for (let i = 0; i < 10; i += 1) {
   milkshake.push({
     id: `food-milkshake-${i}`,
-    x: randomFromRange(0, 2000),
-    y: randomFromRange(0, 2000),
+    // x: randomFromRange(0, 2000),
+    // y: randomFromRange(0, 2000),
+    x: makeXY(),
+    y: makeXY(),
   });
 }
 const snicker = [];
 for (let i = 0; i < 10; i += 1) {
   snicker.push({
     id: `food-snicker-${i}`,
-    x: randomFromRange(0, 2000),
-    y: randomFromRange(0, 2000),
+    // x: randomFromRange(0, 2000),
+    // y: randomFromRange(0, 2000),
+    x: makeXY(),
+    y: makeXY(),
   });
 }
 
--- assets_factory/skill_factory.js
+++ assets_factory/skill_factory.js
@@ -1,27 +1,33 @@
-const randomFromRange = require('./asset_factory_util');
+const { randomFromRange, makeXY } = require('./asset_factory_util');
 
 const slow = [];
 for (let i = 0; i < 10; i += 1) {
   slow.push({
     id: `skills-slow-${i}`,
-    x: randomFromRange(0, 2000),
-    y: randomFromRange(0, 2000),
+    // x: randomFromRange(0, 2000),
+    // y: randomFromRange(0, 2000),
+    x: makeXY(),
+    y: makeXY(),
   });
 }
 const fast = [];
 for (let i = 0; i < 10; i += 1) {
   fast.push({
     id: `skills-fast-${i}`,
-    x: randomFromRange(0, 2000),
-    y: randomFromRange(0, 2000),
+    // x: randomFromRange(0, 2000),
+    // y: randomFromRange(0, 2000),
+    x: makeXY(),
+    y: makeXY(),
   });
 }
 const teleport = [];
 for (let i = 0; i < 10; i += 1) {
   teleport.push({
     id: `skills-teleport-${i}`,
-    x: randomFromRange(0, 2000),
-    y: randomFromRange(0, 2000),
+    // x: randomFromRange(0, 2000),
+    // y: randomFromRange(0, 2000),
+    x: makeXY(),
+    y: makeXY(),
     toX: randomFromRange(0, 2000),
     toY: randomFromRange(0, 2000),
   });
