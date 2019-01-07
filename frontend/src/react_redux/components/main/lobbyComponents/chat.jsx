2a5635dc67e9f0e83b08c6e25981b8d9c11214c0
FilippKramer <krame490@gmail.com>
2019-01-06 20:22:07 -0800

    Fix bugs on chat.jsx

--- frontend/src/react_redux/components/main/lobbyComponents/chat.jsx
+++ frontend/src/react_redux/components/main/lobbyComponents/chat.jsx
@@ -3,31 +3,29 @@ import io from 'socket.io-client';
 import { connect } from 'react-redux';
 
 
-const msp = state => {
-  messages: state.messages
-}
+const msp = state => ({
+  messages: state.messages,
+});
 
 class Chat extends Component {
   constructor(props) {
-    super(props) {
-      this.state = {
-        message: ''
-      }
-      this.socket = io('localhost:5000');
-    }
-
+    super(props);
+    this.state = {
+      message: '',
+    };
+    this.socket = io('localhost:5000');
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    this.setState({message: event.target.value})
+    this.setState({ message: event.target.value });
   }
 
   handleSubmit(event) {
     event.preventDefault('');
-    this.socket.emit("chat message", this.state.message)
-    this.setState({message: ''})
+    this.socket.emit('chat message', this.state.message);
+    this.setState({ message: '' });
   }
 
   render() {
@@ -37,11 +35,11 @@ class Chat extends Component {
         <ul id="messages" />
         <form onSubmit={this.handleSubmit}>
           <input id="messages" />
-          <input type="submit" value="Send"/>
+          <input type="submit" value="Send" />
         </form>
       </div>
     );
   }
 }
 
-export default (msp)(Chat);
+export default connect(msp)(Chat);
