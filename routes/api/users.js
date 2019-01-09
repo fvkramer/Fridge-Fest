a7b1d61b69ea6118374d76a7c5321de43dc999ed
FilippKramer <krame490@gmail.com>
2019-01-08 18:31:53 -0800

    Fix login on backend

--- routes/api/users.js
+++ routes/api/users.js
@@ -49,7 +49,7 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-  const { errors, isValid } = validateRegisterInput(req.body);
+  const { errors, isValid } = validateLoginInput(req.body);
 
   if (!isValid) {
     return res.status(400).json(errors);
--- validation/login.js
+++ validation/login.js
@@ -2,22 +2,22 @@ const Validator = require('validator');
 const validText = require('./valid-text');
 
 module.exports = function validateLoginInput(data) {
-  let errors = {};
+  const errors = {};
 
   data.username = validText(data.username) ? data.username : '';
   data.password = validText(data.password) ? data.password : '';
 
-  if (!Validator.isEmpty(data.username)) {
+  if (Validator.isEmpty(data.username)) {
     errors.username = 'Username is empty';
   }
 
-  if (!Validator.isEmpty(data.password)) {
+  if (Validator.isEmpty(data.password)) {
     errors.password = 'Password is empty';
   }
 
 
   return {
     errors,
-    isValid: Object.keys(errors).length === 0
+    isValid: Object.keys(errors).length === 0,
   };
 };
