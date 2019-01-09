40334d0696d34737570b46a8b9068c18bae236f6
FilippKramer <krame490@gmail.com>
2019-01-09 08:57:32 -0800

    Add more skill items

--- assets_factory/skill_factory.js
+++ assets_factory/skill_factory.js
@@ -2,7 +2,7 @@ const { makeXY } = require('./asset_factory_util');
 
 const makeSkills = () => {
   const slow = [];
-  for (let i = 0; i < 5; i += 1) {
+  for (let i = 0; i < 10; i += 1) {
     slow.push({
       id: `skills-slow-${i}`,
       x: makeXY().x,
@@ -10,7 +10,7 @@ const makeSkills = () => {
     });
   }
   const fast = [];
-  for (let i = 0; i < 5; i += 1) {
+  for (let i = 0; i < 10; i += 1) {
     fast.push({
       id: `skills-fast-${i}`,
       x: makeXY().x,
@@ -18,7 +18,7 @@ const makeSkills = () => {
     });
   }
   const teleport = [];
-  for (let i = 0; i < 5; i += 1) {
+  for (let i = 0; i < 10; i += 1) {
     teleport.push({
       id: `skills-teleport-${i}`,
       x: makeXY().x,
@@ -28,7 +28,7 @@ const makeSkills = () => {
     });
   }
   const lightning = [];
-  for (let i = 0; i < 5; i += 1) {
+  for (let i = 0; i < 10; i += 1) {
     lightning.push({
       id: `skills-lightning-${i}`,
       x: makeXY().x,
