f0b0ec7de599ec56c15f5acafd9892f2e3f7c73e
FilippKramer <krame490@gmail.com>
2019-01-30 16:52:36 -0800

    Add demo component and route to root

--- frontend/src/react_redux/components/app.jsx
+++ frontend/src/react_redux/components/app.jsx
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 // import { AuthRoute } from '../util/route_util';
 import Canvas from './canvas';
 import Lobby from './main/lobby';
+import Demo from './demo';
 
 import MainPageContainer from './main/main_page_container';
 
@@ -11,6 +12,7 @@ const App = () => (
     <Switch>
       <Route exact path="/lobby" component={Lobby} />
       <Route exact path="/game" component={Canvas} />
+      <Route exact path="/demo" component={Demo}
       <Route path="/" component={MainPageContainer} />
     </Switch>
   </div>
