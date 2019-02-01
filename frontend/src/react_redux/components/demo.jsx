117bb05d800800df9e5afb81bafb7afc076e8e5f
FilippKramer <krame490@gmail.com>
2019-02-01 15:53:28 -0800

    Style play button

--- frontend/src/react_redux/components/demo.jsx
+++ frontend/src/react_redux/components/demo.jsx
@@ -18,10 +18,11 @@ export default class Demo extends Component {
 
         <iframe className="vid" width="645" height="400" src="https://www.youtube.com/embed/UXHIhfwP4Iw" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
 
-        <div>
+        <div className="options">
           <Link to="/"><button className="home-button">Home</button></Link>
           <Link to="/game"><button className="home-button" onClick={this.handleGame}>Play</button></Link>
         </div>
+
       </div>
     );
   }
